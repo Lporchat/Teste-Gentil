616129fd84b6083bd82bf56cc9a70f99efbaacdc
Leonardo Monteiro <lporchat06@gmail.com>
2024-08-09 16:05:11 -0300

    Add envs no projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/index');
 const PessoaController = require('../controller/pessoa');
 const validateCPF = require('../utils/valideteCPF');
 
